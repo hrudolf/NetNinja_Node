f9492b2640cae0c07f606ff2605f9fe3773dbe34
hrudolf <h.rudolf@gmail.com>
2022-11-30 13:53:37 +0100

    Dynamic content with ejs

--- app.js
+++ app.js
@@ -3,20 +3,38 @@ const express = require('express');
 //express app
 const app = express();
 
-//listen for requests
-app.listen(3000)
+//register view engine
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+    const blogs = [
+        {title: "Rogues do it from behind", snippet: "Cheap shot, backstab, backstab, kidney shot, backstab, backstab, eviscerate"},
+        {title: "Warlock is a mushroom class", snippet: "Fear resistance increased for all classes by 95%"},
+        {title: "Warrior buff nerfed", snippet: "500 hps trinket nerfed to 495 hps."},
+    ];
+
     //res.send('<p>home page</p>')
-    res.sendFile('./views/index.html', { root: __dirname });
-}).get('/about', (req, res) => {
+    res.render('index', {title: 'Home', blogs}); //vagy: blogs: blogs
+})
+
+app.get('/about', (req, res) => {
     //res.send('<p>about page</p>')
-    res.sendFile('./views/about.html', { root: __dirname });
-}).get('/about-us', (req, res) => {
+    res.render('about', {title: 'About'});
+})
+
+app.get('/about-us', (req, res) => {
     res.redirect('/about');
+})
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create a new blog'});
 })
 
 //404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    res.status(404).render('404', {title: '404'});
 })
+
+
+//listen for requests
+app.listen(3000, () => console.log("listening on port 3000"));
